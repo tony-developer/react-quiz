f77d9d439f5d47cb7a7a3858cf725cf84fccb34d
tony-developer <anton.serebriackoff@yandex.ru>
2020-11-04 12:00:40 +0300

    added routing

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.module.css'
 import Button from '../UI/Button/Button'
+import {Link} from "react-router-dom"
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -37,7 +38,10 @@ const FinishedQuiz = props => {
 
         <div>
           <Button onClick={props.onRetry} type="primary">Повторить</Button>
-          <Button type="success">Перейти в список тестов</Button>
+          <Link to="/">
+            <Button type="success">Перейти в список тестов</Button>
+          </Link>
+
         </div>
       </div>
   )
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+const app = (
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
+)
+
+
+ReactDOM.render(
+  // <React.StrictMode>
+    app,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
